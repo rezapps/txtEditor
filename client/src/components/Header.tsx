361fd3e149e0e4350f58eaba2c834b5e0521d2e0
rezaapps <jus3p3@proton.me>
2023-03-22 12:50:27 +0100

    fixed header links

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,48 +1,18 @@
 import { Link, useNavigate } from "react-router-dom";
-import { useAuth0 } from "@auth0/auth0-react";
 
 const Header = () => {
-
-
-	const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0()
-	// const { picture } = user
-
 	return (
 		<header className="header">
 			<div className="logo">
 				<Link to="/">TxtEditor</Link>
 			</div>
-
-
-			{!isAuthenticated ?
-				(<ul className="nav">
-					<li
-						onClick={() => loginWithRedirect}
-					>
-						Login
-					</li>
-					<li>
-						<Link to="/signup"
-							onClick={() =>
-								loginWithRedirect({
-							})}
-						>
-							Register
-							</Link>
-					</li>
-				</ul>) :
-				(
-					<ul className="nav">
-						<li
-							onClick={() => logout({returnTo: window.location.origin})}
-						>
-							Logout
-						</li>
-					</ul>
-				)
-			}
-
-
+			<ul className="nav">
+				<li>Logout</li>
+				<li>Login</li>
+				<li>
+					<Link to="/signup">Register</Link>
+				</li>
+			</ul>
 		</header>
 	)
 }
